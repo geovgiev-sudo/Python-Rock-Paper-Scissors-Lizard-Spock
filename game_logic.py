95955bb379eb85ce017fe2782ecb00aba402a11d
Georgi Georgiev <geovgiev@gmail.com>
2026-01-20 11:48:22 +0200

    Updates

--- game_logic.py
+++ game_logic.py
@@ -19,7 +19,18 @@ draw_counter = 0
 lose_counter = 0
 troll_counter = 0
 
-print(f'\n{BOLD}{BLUE}Hello and welcome to "Fun with ROCK, PAPER, SCISSORS, LIZARD or SPOCK!"\n')
+print(f'\n{BOLD}{BLUE}Hello and welcome to "Fun with ROCK, PAPER, SCISSORS, LIZARD or SPOCK!"\n'
+      f'Here are the game rules:'
+      f'Scissors cuts Paper'
+      f'Paper covers Rock'
+      f'Rock crushes Lizard'
+      f'Lizard poisons Spock'
+      f'Spock smashes Scissors'
+      f'Scissors decapitates Lizard'
+      f'Lizard eats Paper'
+      f'Paper disproves Spock'
+      f'Spock vaporizes Rock'
+      f'(and as it always has) Rock crushes Scissors')
 input('Press ENTER to continue and have fun! ')
 game_mode = input(f'\n{MAGENTA}Would you like to play against the computer or another human? Type "COMPUTER" or "HUMAN" ').lower()
 
